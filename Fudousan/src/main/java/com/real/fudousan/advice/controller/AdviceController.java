7d49fa2f2a844095695a908113f53ef92fa72935
SCIT-PC\SCIT <kangkimu@gmail.com>
2018-03-27 19:45:58 +0900

    Advice 클래스 어노테이션 추가

--- Fudousan/src/main/java/com/real/fudousan/advice/controller/AdviceController.java
+++ Fudousan/src/main/java/com/real/fudousan/advice/controller/AdviceController.java
@@ -1,5 +1,12 @@
 package com.real.fudousan.advice.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.real.fudousan.advice.service.AdviceService;
+
 public class AdviceController {
+	
+	@Autowired
+	private AdviceService service;
 
 }
--- Fudousan/src/main/java/com/real/fudousan/advice/dao/AdviceDAOOracle.java
+++ Fudousan/src/main/java/com/real/fudousan/advice/dao/AdviceDAOOracle.java
@@ -1,5 +1,8 @@
 package com.real.fudousan.advice.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AdviceDAOOracle implements AdviceDAO {
 
 }
