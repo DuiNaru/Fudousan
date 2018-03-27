90dba70ba8c02d7fd21a38b2315958e2e0007bf7
LAPTOP-PTI4RN7L\SCIT <kebinkastora@naver.com>
2018-03-27 15:54:40 +0900

    mybatis-config.xml change
    03-27 - 15:54

--- Fudousan/src/main/java/com/real/fudousan/estate/dao/EstateDAO.java
+++ Fudousan/src/main/java/com/real/fudousan/estate/dao/EstateDAO.java
@@ -2,6 +2,8 @@ package com.real.fudousan.estate.dao;
 
 import java.util.ArrayList;
 
+import com.real.fudousan.estate.vo.Estate;
+
 public interface EstateDAO {
-	public ArrayListt<Estate> toto();
+	
 }
