880083ea5478de3a6d62466dbca16257e2aea906
안해민 <kebinkastora@naver.com>
2018-04-12 15:59:58 +0900

    뒤로가기 구현하기

--- Fudousan/src/main/webapp/WEB-INF/views/anhaeminTest.jsp
+++ Fudousan/src/main/webapp/WEB-INF/views/anhaeminTest.jsp
@@ -25,14 +25,6 @@
 <body>
 <div id="myButton">
 <input id="sendMsg"/><button id="chatBtn">챗고!</button>
-	<button id="Btn_object" type="button">객체 소환</button>
-	<button id="Btn_blist" type="button">array1 보기</button>
-	<button id="Btn_blist2" type="button">array2 보기</button>
-	<button id="Btn_backArray1" type="button">뒤로가기</button>
-	<button id="Btn_clear" type="button">array 1초기화</button>
-	<button id="Btn_blistRecovery" type="button">앞으로가기</button><br>
-	<button id="Btn_realSelect" type="button">리얼셀렉트버튼</button>
-	<button id="Btn_addGeometry" type="button">바닥색 변경</button>
     <ul id="msg"></ul>
 </div>
 <div id="threejsShow">
@@ -46,78 +38,14 @@
 	  
 		var socket = io('http://localhost:7000');
 		var chatBtn = document.querySelector('#chatBtn');
-		var Btn_object = document.querySelector('#Btn_object');
-		var Btn_blist = document.querySelector('#Btn_blist');
-		var Btn_blist2 = document.querySelector('#Btn_blist2');
-		var Btn_blistRecovery = document.querySelector('#Btn_blistRecovery');
-		var Btn_backarray1 = document.querySelector('#Btn_backArray1');
-		var Btn_clear = document.querySelector('#Btn_clear');
-		var Btn_realSelect = document.querySelector('#Btn_realSelect');
-		var Btn_addGeometry = document.querySelector('#Btn_addGeometry');
-		var cocoa_count = 0;
 
-	//지오매트리 영역	
+	
 		chatBtn.addEventListener("click", function(event) {
 			socket.emit('chat message', $('#sendMsg').val());
 		});
 		
-		Btn_object.addEventListener("click", function(event) {
-			socket.emit('abc', {name : 'anhaemin'});
-		});
-		
-		Btn_blist.addEventListener("click", function(event) {
-			socket.emit('call list');
-		});
-		
-		Btn_blist2.addEventListener("click", function(event) {
-			socket.emit('call list2');
-		});
-		
-		Btn_backArray1.addEventListener('click', function(event){
-			socket.emit('back_array');
-		});
-		Btn_clear.addEventListener('click', function(event){
-			socket.emit('clear_array1');
-		})
-		
-		Btn_blistRecovery.addEventListener('click', function(event){
-			socket.emit('Array1_Recovery');
-		});
-		
-		Btn_realSelect.addEventListener('click', function(event){
-			socket.emit('realSelect');
-		});
-		
 		socket.on('chat message', function(msg){
 			$('#msg').append($('<li>').text(msg));
-		});
-		
-		//바닥 색 바꾸기
-		Btn_addGeometry.addEventListener('click', function(event){
-			socket.emit('planeChange');
-		});
-		
-		socket.on('cocoa', function(){
-			console.log('cocoa - Start');
-			console.time('cocoa_time');
-			scene.remove(plane);
-			var planeGeometry = new THREE.PlaneGeometry(5000, 6000);
-			var planeMaterial = new THREE.MeshBasicMaterial({color:0xb5f441, sid:THREE.DoubleSice});
-			plane = new THREE.Mesh(planeGeometry, planeMaterial);
-			scene.add(plane);
-			
-			cocoa_count += 1;
-			var arrayOkSign = ['퍼스트','세컨드'];
-			if(cocoa_count == 1){
-				var sendSign = arrayOkSign[0];
-			}else{
-				var sendSign = arrayOkSign[1];
-			}
-			socket.emit('sameTimeChange_check', sendSign);
-			console.timeEnd('cocoa_time');
-			
-			
-			console.log('cocoa - End');
 		});
 		
 		socket.on('planeChangeOkMassage', function(data){
--- Fudousan/src/main/webapp/resources/js/prototype/main.js
+++ Fudousan/src/main/webapp/resources/js/prototype/main.js
@@ -128,22 +128,46 @@ function init() {
 				  console.log('red');
 			  },
 			  의자추가 : function(){
-				  console.log('의자를 추가했어요');
+				  console.log('의자가 생성됩니다.')
+					// 외부 모델 로더 생성
+					const loader = new THREE.TDSLoader();
+					// 해당 모델의 텍스쳐 경로 설정
+					loader.setPath("/fudousan/resources/model/testchair/");
+					// 모델 데이터 경로 설정 및 로딩 완료시 리스너 지정
+					loader.load("/fudousan/resources/model/testchair/Armchair.3ds", (object) => {
+						// x축 기준으로 -90도 회전
+						object.rotation.x = Math.PI * -90 / 180;
+
+						// 해당 모델을 가장 가깝게 에워싸는 육면체인 BoundingBox 생성
+						var boundingBox = new THREE.Box3();
+						boundingBox.setFromObject(object);
+
+						// 바운딩 박스의 z 값을 이용하여 이동
+						object.position.z = boundingBox.max.z;
+						// 화면에 추가
+						scene.add(object);
+						// 완료 Alert 띄움
+						alert("Complete");
+
+						objects.push(object);	// 해당 object는 Groups 객체로써 DragControls와 호환 X
+						chair = object;
+					});
 			  },
 			  뒤로가기 : function(){
-				  console.log('뒤로갔어요');
+				  console.log('뒤로가기를 눌렀습니다.');
+				 socket.emit('array_back');
 			  },
 			  앞으로가기 : function(){
-				  console.log('앞으로갔어요');
+				  console.log('앞으로가기 눌렀습니다.');
 			  },
 			  저장 : function(){
-				  console.log('저장했어요');
+				  console.log('저장하기 눌렀습니다.');
 			  },
 			  초기화 : function(){
-				  console.log('초기화했어요');
+				  console.log('초기화 눌렀습니다.');
 			  },
 			  종료 : function(){
-				  console.log('종료햇어요');
+				  console.log('종료눌렀습니다');
 			  },
 			  아이템제거하기 : function(){
 				  console.log('이제 클릭하면 제거됨');
@@ -172,26 +196,41 @@ function init() {
 			
 			
 	//socket 통신 부분
+	socket.on('whoAreYou',function(data){
+		whoAmI = data;
+	});
+	
+	socket.on('goback', function(data){
+		var abc = data;
+		switch(abc){
+		case 'bluePlane' : console.log(abc); //뒤로가기로 파란바닥 생성이 왔으니까 파란바닥을 생성해야한다.
+			socket.emit('bluePlane', 'goback');
+			break;
+		case 'greenPlane' : console.log(abc); //뒤로가기로 녹색바닥 생성이 왔으니까  녹색바닥을 생성해야한다.
+			socket.emit('greenPlane','goback'); 
+			break;
+		default : break;
+		};
+	
+	});
 	
 	//바닥색을 blue로 바꾸는 메소드
-	socket.on('ServiceCall_bluePlane', function(){
-		scene.remove(plane);
-		console.time('텍스쳐 로딩 시간 체크 - Start');
-		var planeGeometry = new THREE.PlaneGeometry(5000, 6000);
-		var planeMaterial = new THREE.MeshBasicMaterial({color:0x425ff4, sid:THREE.DoubleSice});
-		plane = new THREE.Mesh(planeGeometry, planeMaterial);
-		scene.add(plane);
-		console.timeEnd('텍스쳐 로딩 시간 체크 - End');
-		saveTimeChange();
+	socket.on('ServiceCall_bluePlane', function(data){
+		blue_floor();
+		var abc = data
+		console.log(abc);
+		if(whoAmI == 'selecter' && data != 'goback'){ // 누른사람이면 selecet && 뒤로가기로 실행하는거면  저장안함
+			socket.emit('goArray1', "bluePlane"); // 배열1에 실행된거 기록하기
+		}
 	});
 	
 	//바닥을 green으로 바꾸는 메소드
-	socket.on('ServiceCall_greenPlane', function(){
+	socket.on('ServiceCall_greenPlane', function(data){
+			
 			green_floor();
-			if(whoAmI == 'selecter'){
-				socket.emit('goArray1', function(){
-					green_floor();
-				});
+			var test = data;
+			if(whoAmI == 'selecter' && test != 'goback'){
+				socket.emit('goArray1', "greenPlane");
 			}
 	});
 	
@@ -204,16 +243,23 @@ function init() {
 		saveTimeChange();
 	};
 	
+	function blue_floor(){
+		scene.remove(plane);
+		var planeGeometry = new THREE.PlaneGeometry(5000, 6000);
+		var planeMaterial = new THREE.MeshBasicMaterial({color:0x425ff4, sid:THREE.DoubleSice});
+		plane = new THREE.Mesh(planeGeometry, planeMaterial);
+		scene.add(plane);
+		saveTimeChange();
+	};
 	
 	
 	
-	socket.on('whoAreYou',function(data){
-		whoAmI = data;
-	});
+	
 	socket.on('interaction', function(data){
 		 alert(data); 
 	});
 	socket.on('successChangeMessage', function(data){
+		console.log(whoAmI);
 		if(whoAmI == 'selecter'){
 			alert('상대방도 선택사항이 적용되었습니다.')
 			whoAmI = "";
